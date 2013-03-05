901fd6a8549b63bcf7c2886db6beeca49184c401
Luka Mirosevic <luka@goonbee.com>
2013-03-05 13:22:13 +0100

    Added method for programatically setting a shortcut which is tied to a NSUserDefaults key

--- MASShortcut+UserDefaults.h
+++ MASShortcut+UserDefaults.h
@@ -4,5 +4,6 @@
 
 + (void)registerGlobalShortcutWithUserDefaultsKey:(NSString *)userDefaultsKey handler:(void (^)())handler;
 + (void)unregisterGlobalShortcutWithUserDefaultsKey:(NSString *)userDefaultsKey;
++ (void)setShortcut:(MASShortcut *)shortcut forUserDefaultsKey:(NSString *)userDefaultsKey;
 
 @end
