756601488fe195b0b14a8ac9c3c418140d5845f0
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2014-08-07 10:26:20 +0200

    Binding options can now be customized for MASShortcutBinder.
    This makes it possible to customize the way the shortcuts are stored
    in user defaults. The default options call for the keyed archiver
    transformer, deserializing the shortcuts from NSData.

--- Framework/MASShortcutBinder.h
+++ Framework/MASShortcutBinder.h
@@ -3,6 +3,7 @@
 @interface MASShortcutBinder : NSObject
 
 @property(strong) MASShortcutMonitor *shortcutMonitor;
+@property(copy) NSDictionary *bindingOptions;
 
 - (void) bindShortcutWithDefaultsKey: (NSString*) defaultsKeyName toAction: (dispatch_block_t) action;
 - (void) breakBindingWithDefaultsKey: (NSString*) defaultsKeyName;
