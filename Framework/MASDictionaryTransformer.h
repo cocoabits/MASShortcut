cd0926808c881bd281443e3bcc3aa422e063cefb
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-01-13 08:47:43 +0100

    Changed headerdoc markup to work better with CocoaDocs (closes #55).

--- Framework/MASDictionaryTransformer.h
+++ Framework/MASDictionaryTransformer.h
@@ -1,17 +1,17 @@
 extern NSString *const MASDictionaryTransformerName;
 
 /**
-    @brief Converts shortcuts for storage in user defaults.
+    Converts shortcuts for storage in user defaults.
 
     User defaults can’t stored custom types directly, they have to
-    be serialized to @p NSData or some other supported type like an
-    @p NSDictionary. In Cocoa Bindings, the conversion can be done
+    be serialized to `NSData` or some other supported type like an
+    `NSDictionary`. In Cocoa Bindings, the conversion can be done
     using value transformers like this one.
 
-    There’s a built-in transformer (@p NSKeyedUnarchiveFromDataTransformerName)
-    that converts any @p NSCoding types to @p NSData, but with shortcuts
+    There’s a built-in transformer (`NSKeyedUnarchiveFromDataTransformerName`)
+    that converts any `NSCoding` types to `NSData`, but with shortcuts
     it makes sense to use a dictionary instead – the defaults look better
-    when inspected with the @p defaults command-line utility and the
+    when inspected with the `defaults` command-line utility and the
     format is compatible with an older sortcut library called Shortcut
     Recorder.
 */
--- Framework/MASShortcutBinder.h
+++ Framework/MASShortcutBinder.h
@@ -1,22 +1,22 @@
 #import "MASShortcutMonitor.h"
 
 /**
-    @brief Binds actions to user defaults keys.
+    Binds actions to user defaults keys.
 
     If you store shortcuts in user defaults (for example by binding
-    a @p MASShortcutView to user defaults), you can use this class to
+    a `MASShortcutView` to user defaults), you can use this class to
     connect an action directly to a user defaults key. If the shortcut
     stored under the key changes, the action will get automatically
     updated to the new one.
 
-    This class is mostly a wrapper around a @p MASShortcutMonitor. It
+    This class is mostly a wrapper around a `MASShortcutMonitor`. It
     watches the changes in user defaults and updates the shortcut monitor
     accordingly with the new shortcuts.
 */
 @interface MASShortcutBinder : NSObject
 
 /**
-    @brief A convenience shared instance.
+    A convenience shared instance.
 
     You may use it so that you don’t have to manage an instance by hand,
     but it’s perfectly fine to allocate and use a separate instance instead.
@@ -24,23 +24,23 @@
 + (instancetype) sharedBinder;
 
 /**
-    @brief The underlying shortcut monitor.
+    The underlying shortcut monitor.
 */
 @property(strong) MASShortcutMonitor *shortcutMonitor;
 
 /**
-    @brief Binding options customizing the access to user defaults.
+    Binding options customizing the access to user defaults.
 
-    As an example, you can use @p NSValueTransformerNameBindingOption to customize
+    As an example, you can use `NSValueTransformerNameBindingOption` to customize
     the storage format used for the shortcuts. By default the shortcuts are converted
-    from @p NSData (@p NSKeyedUnarchiveFromDataTransformerName). Note that if the
-    binder is to work with @p MASShortcutView, both object have to use the same storage
+    from `NSData` (`NSKeyedUnarchiveFromDataTransformerName`). Note that if the
+    binder is to work with `MASShortcutView`, both object have to use the same storage
     format.
 */
 @property(copy) NSDictionary *bindingOptions;
 
 /**
-    @brief Binds given action to a shortcut stored under the given defaults key.
+    Binds given action to a shortcut stored under the given defaults key.
 
     In other words, no matter what shortcut you store under the given key,
     pressing it will always trigger the given action.
@@ -48,19 +48,19 @@
 - (void) bindShortcutWithDefaultsKey: (NSString*) defaultsKeyName toAction: (dispatch_block_t) action;
 
 /**
-    @brief Disconnect the binding between user defaults and action.
+    Disconnect the binding between user defaults and action.
 
     In other words, the shortcut stored under the given key will no longer trigger an action.
 */
 - (void) breakBindingWithDefaultsKey: (NSString*) defaultsKeyName;
 
 /**
-    @brief Register default shortcuts in user defaults.
+    Register default shortcuts in user defaults.
 
-    This is a convenience frontent to [NSUserDefaults registerDefaults].
+    This is a convenience frontent to `[NSUserDefaults registerDefaults]`.
     The dictionary should contain a map of user defaults’ keys to appropriate
     keyboard shortcuts. The shortcuts will be transformed according to
-    @p bindingOptions and registered using @p registerDefaults.
+    `bindingOptions` and registered using `registerDefaults`.
 */
 - (void) registerDefaultShortcuts: (NSDictionary*) defaultShortcuts;
 
