bf3a032c4b9205437003a7decca66bd6533fc8bb
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-01-16 12:57:08 +0100

    Decrease headerdoc indenting to appease appledoc (see #55).
    I think I have finally found out the reason for CocoaDocs ignoring
    our markup: I have indented the documentation by four spaces, which
    was interpreted as “code” by appledoc. Trying now without the indent,
    that should finally help.

--- Framework/MASShortcutMonitor.h
+++ Framework/MASShortcutMonitor.h
@@ -1,11 +1,11 @@
 #import "MASShortcut.h"
 
 /**
-    Executes action when a shortcut is pressed.
+ Executes action when a shortcut is pressed.
 
-    There can only be one instance of this class, otherwise things
-    will probably not work. (There’s a Carbon event handler inside
-    and there can only be one Carbon event handler of a given type.)
+ There can only be one instance of this class, otherwise things
+ will probably not work. (There’s a Carbon event handler inside
+ and there can only be one Carbon event handler of a given type.)
 */
 @interface MASShortcutMonitor : NSObject
 
@@ -13,10 +13,10 @@
 + (instancetype) sharedMonitor;
 
 /**
-    Register a shortcut along with an action.
+ Register a shortcut along with an action.
 
-    Attempting to insert an already registered shortcut probably won’t work.
-    It may burn your house or cut your fingers. You have been warned.
+ Attempting to insert an already registered shortcut probably won’t work.
+ It may burn your house or cut your fingers. You have been warned.
 */
 - (BOOL) registerShortcut: (MASShortcut*) shortcut withAction: (dispatch_block_t) action;
 - (BOOL) isShortcutRegistered: (MASShortcut*) shortcut;
