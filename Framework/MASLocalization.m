a73a254ce717e22eaa17764d8516020177fd6034
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-10-08 11:30:24 +0200

    Documented the CocoaPods localization fix.

--- Framework/MASLocalization.m
+++ Framework/MASLocalization.m
@@ -1,6 +1,10 @@
 #import "MASLocalization.h"
 #import "MASShortcut.h"
 
+// The CocoaPods trickery here is needed because when then code
+// is built as a part of CocoaPods, it won’t make a separate framework
+// and the Localized.strings file won’t be bundled correctly.
+// See https://github.com/shpakovski/MASShortcut/issues/74
 NSString *MASLocalizedString(NSString *key, NSString *comment) {
     NSBundle *frameworkBundle = nil;
 #ifdef COCOAPODS
