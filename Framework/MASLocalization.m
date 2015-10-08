a0af086f16597146db71e1339c6cd2a55a00ff1b
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-10-08 11:53:21 +0200

    Typo fix.

--- Framework/MASLocalization.m
+++ Framework/MASLocalization.m
@@ -1,7 +1,7 @@
 #import "MASLocalization.h"
 #import "MASShortcut.h"
 
-// The CocoaPods trickery here is needed because when then code
+// The CocoaPods trickery here is needed because when the code
 // is built as a part of CocoaPods, it won’t make a separate framework
 // and the Localized.strings file won’t be bundled correctly.
 // See https://github.com/shpakovski/MASShortcut/issues/74
