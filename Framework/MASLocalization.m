e267d30bfd212e1261fef9ca84fd7376d8ca21e9
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2016-01-09 15:45:45 +0100

    Whitespace fixes.

--- Framework/MASLocalization.m
+++ Framework/MASLocalization.m
@@ -13,20 +13,20 @@ NSString *MASLocalizedString(NSString *key, NSString *comment) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         NSBundle *frameworkBundle = [NSBundle bundleForClass:[MASShortcut class]];
-		// first we'll check if resources bundle was copied to MASShortcut framework bundle when !use_frameworks option is active
-		NSURL *cocoaPodsBundleURL = [frameworkBundle URLForResource:@"MASShortcut" withExtension:@"bundle"];
-		if (cocoaPodsBundleURL) {
-			localizationBundle = [NSBundle bundleWithURL: cocoaPodsBundleURL];
-		} else {
-			// trying to fetch cocoapods bundle from main bundle
-			cocoaPodsBundleURL = [[NSBundle mainBundle] URLForResource: @"MASShortcut" withExtension:@"bundle"];
-			if (cocoaPodsBundleURL) {
-				localizationBundle = [NSBundle bundleWithURL: cocoaPodsBundleURL];
-			} else {
-				// fallback to framework bundle
-				localizationBundle = frameworkBundle;
-			}
-		}
+        // first we'll check if resources bundle was copied to MASShortcut framework bundle when !use_frameworks option is active
+        NSURL *cocoaPodsBundleURL = [frameworkBundle URLForResource:@"MASShortcut" withExtension:@"bundle"];
+        if (cocoaPodsBundleURL) {
+            localizationBundle = [NSBundle bundleWithURL: cocoaPodsBundleURL];
+        } else {
+            // trying to fetch cocoapods bundle from main bundle
+            cocoaPodsBundleURL = [[NSBundle mainBundle] URLForResource: @"MASShortcut" withExtension:@"bundle"];
+            if (cocoaPodsBundleURL) {
+                localizationBundle = [NSBundle bundleWithURL: cocoaPodsBundleURL];
+            } else {
+                // fallback to framework bundle
+                localizationBundle = frameworkBundle;
+            }
+        }
     });
     return [localizationBundle localizedStringForKey:key
         value:MASPlaceholderLocalizationString
